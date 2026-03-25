5c8e7417386bb20bf9871c06528f4133379eb591
모하영 <mohayoung80@gmail.com>
2026-03-25 13:58:19 +0900

    게시판 하위폴더 삭제
    자유게시판 하위폴더 list 제외하고 삭제하였습니다

--- mat_client/src/App.js
+++ mat_client/src/App.js
@@ -4,9 +4,8 @@ import MapPage from './map/MapPage';
 import SearchPage from './totSearch/SearchPage';
 import LoginPage from './login/LoginPage';
 import UserMyPage from './myPage/user/UserMyPage';
-import BoardPage from './board/BoardPage';
 import CityHome from './cityHome/CityHome';
-// import AdminPage from './myPage/admin/AdminPage';
+import AdminPage from './myPage/admin/AdminPage';
 
 function App() {
   return (
@@ -17,9 +16,8 @@ function App() {
         <Route path="/search" element={<SearchPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/mypage" element={<UserMyPage />} />
-        <Route path="/board" element={<BoardPage />} />
         <Route path="/cityhome" element={<CityHome />} />
-        {/* <Route path="/admin" element={<AdminPage />} /> */}
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
