fddee16766a5ae87aab1c7c6837d4e6ea0c7da1c
모하영 <mohayoung80@gmail.com>
2026-03-26 16:06:39 +0900

    보드 스키마 변경 완료
    로직에 맞게 수정하였습니다.

--- models/FreeBoardSchema.js
+++ models/FreeBoardSchema.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 const FreeBoardSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  title: { type: String},
+  title: { type: String, required: true },
   subject: { type: String, required: true },
 
   //article스키마 참조
   images: [
     {
-      saveFileName:     { type: String, required: true },
-      originalFileName: { type: String, required: true }
+      saveFileName: { type: String },
+      originalFileName: { type: String }
     }
   ],
-  type:{ type: String}
+  type: { type: String, default: 'text' }
 });
 //모델 중복으로 인해서 exportt만 살려뒀습니다
-console.log('자유게시판 정의')
+console.log('자유게시판 정의 완료 - 모든 모드 통합형')
 
 module.exports = mongoose.model('freeboards', FreeBoardSchema);
