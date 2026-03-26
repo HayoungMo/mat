a2491ecdc5eb64a55e6bc8f26cc9a523620b4d95
yen1 <yen1k1m124@naver.com>
2026-03-26 18:13:38 +0900

    services 및 routers 수정
    용도에 맞게 services수정
    
    -> article에 동기 처리 방식을 사용하기 위해 형식 수정,
    
    -> upgrade를 admin이 아닌 cityuser에서 사용하는 방식으로 수정, 이에 맞게 update와 단일 검색 등 필요한 내용 추가 (등업 js페이지는 수정이 끝나지 않아 보내지 않았습니다.

--- routes/upgradeRoutes.js
+++ routes/upgradeRoutes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //DB 작명
-const Upgrade = mongoose.model('upgrade')
+const Upgrade = mongoose.model('upgradeRequests')
 
 //미들 웨어 생성하기
 
@@ -12,15 +12,31 @@ module.exports= (app)=>{
         return res.status(200).send(upgradeUser)
     })
 
+    app.get('/api/upgrade/id',async(req,res)=>{
+        const userId = req.body.userId
+        const upgradeUser = await Upgrade.findOne({userId})
+        return res.status(200).send(upgradeUser)
+    })
+
     // 데이터 입력
     app.post('/api/upgrade',async(req,res)=>{
-        const upgradeUser = await Upgrade.find()
+        const upgradeUser = await Upgrade.create(req.body)
         return res.status(200).send({
             error:false,
             upgradeUser
         })
     })
 
+    // 데이터 수정
+    app.put('/api/upgrade',async(req,res)=>{
+        const userId = req.body.userId
+        const upgradeUser = await Upgrade.findByIdAndUpdate(userId)
+        return res.status(200).send({
+            error:false,
+            upgradeUser
+        })
+    })
+    
     // 데이터 삭제
     app.delete('/api/upgrade',async(req,res)=>{
         const userId = req.body.userId
