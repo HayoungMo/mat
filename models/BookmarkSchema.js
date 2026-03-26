0f4d40202e20ef345526141e9d5ba1b113dcc66f
모하영 <mohayoung80@gmail.com>
2026-03-26 11:42:04 +0900

    메인화면을 깔끔하게 지웠습니다
    예니님이 메인에 버튼을 만들어뒀기때문에 거기서 확인하셔도 괜찮습니다

--- models/BookmarkSchema.js
+++ models/BookmarkSchema.js
@@ -4,6 +4,7 @@ const BookmarkSchema = mongoose.Schema({
   userId:    { type: String, required: true },
   articleNo: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
+  //맛집이름, 맛집주소 추가,맛집 전화번호.
 });
 
 module.exports = mongoose.model('bookmarks', BookmarkSchema);
