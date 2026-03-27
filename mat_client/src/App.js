3ea0d58b130bd58e34601af53ba63dae014d817c
dlsghsla4623-cloud <dlsghsla4623@gmail.com>
2026-03-27 12:09:49 +0900

    2026.03.27 김인호 로그인페이지 수정 app 페이지 수정
    세련 코드 추가

--- mat_client/src/App.js
+++ mat_client/src/App.js
@@ -6,31 +6,35 @@ import LoginPage from './login/LoginPage';
 import MyPage from './myPage/MyPage';
 //import CityHome from './myPage/cityUser/CityPage';
 import Board from './board/boardList/Board';
+import React, { useState } from 'react'; // ★ React와 useState 추가
 
 function App() {
 
-  
+  // 추가된 코드: 로그인 유저 상태 (세션 유지용)
+  const [loginUser, setLoginUser] = useState(localStorage.getItem('userId'));
+
   return (
     <div>
       <p>
         <Link to='/'><button>메인</button></Link>
         <Link to='/map'><button>지도</button></Link>
         <Link to='/search'><button>검색</button></Link>
-        <Link to='/login'><button>로그인</button></Link>
+        {/* 추가된코드 */}
+        <Link to='/login'><button>{loginUser ? `${loginUser}님` : '로그인'}</button></Link>
         <Link to='/mypage'><button>마이 페이지</button></Link>
         {/* <Link to='/cityhome'><button>지역 모음</button></Link> */}
         <Link to='/board'><button>게시판</button></Link>
-      
-
       </p>
+
       <Routes>
         <Route path="/" element={<MainPage/>} exact></Route>
         <Route path="/map" element={<MapPage/>} ></Route>
         <Route path="/search" element={<SearchPage/>} ></Route>
-        <Route path="/login" element={<LoginPage/>} ></Route>
-        <Route path="/mypage" element={<MyPage/>} ></Route>
+        <Route path="/login" element={<LoginPage loginUser={loginUser} setLoginUser={setLoginUser} />} ></Route>
+        {/* 팀원 로그인정보 전달코드 */}
+        <Route path="/mypage" element={<MyPage loginUser={loginUser} />} ></Route>
         {/* <Route path="/cityhome" element={<CityHome/>} ></Route> */}
-        <Route path="/board" element={<Board/>} ></Route>
+        <Route path="/board" element={<Board loginUser={loginUser} />} ></Route>
       </Routes>
     </div>
   );
