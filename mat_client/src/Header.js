af7c2ac0a36650862f74c40930ec1245b8889d8e
모하영 <mohayoung80@gmail.com>
2026-04-01 11:57:35 +0900

    작업중 임시 커밋

--- mat_client/src/Header.js
+++ mat_client/src/Header.js
@@ -37,57 +37,56 @@ const Header = ({loginUser,onLogout}) => {
 
     return(
          <header className="auth-header">
-            <div className="header-inner">
+            {/* 1줄: 로고 + 로그인 정보 */}
+            <div className="header-top">
                 <Link to="/" className="logo-text">MAT</Link>
+                <div className="header-right">
+                    {loginUser ? (
+                        <>
+                            <span>{loginUser}님</span>
+                            <span className="bar">|</span>
+                            <Link to="/mypage">마이페이지</Link>
+                            <span className="bar">|</span>
+                            <span onClick={onLogout}>로그아웃</span>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login">로그인</Link>
+                            <span className="bar">|</span>
+                            <Link to="/login">회원가입</Link>
+                        </>
+                    )}
+                </div>
+            </div>
 
-                <div className="header-right-group">
-                    <nav className="header-nav">
-                        <Link to="/">메인</Link>
-                        <Link to="/map">지도</Link>
-                        <Link to="/search">검색</Link>
-                        <Link to="/login">로그인</Link>
-                        <Link to="/mypage">마이페이지</Link>
-                        <Link to="/city">블로그</Link>
-                        <Link to="/board">게시판</Link>
-                    </nav>
+            {/* 2줄: 네비게이션 중앙 + 검색창 */}
+            <div className="header-bottom">
+                <nav className="header-nav">
+                    <Link to="/map">지도</Link>
+                    <Link to="/city">블로그</Link>
+                    <Link to="/board">게시판</Link>
+                </nav>
 
         {/* 검색 영역 */}
-           <div style={{ position: 'relative', marginLeft: '20px' }}>
-                        <input
-                            value={inputText}
-                            onChange={(e) => setInputText(e.target.value)}
-                            onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
-                            placeholder="검색어 입력"
-                            className="header-search-input"
-                        />
-            <button onClick={handleSearch} className="header-search-btn">검색</button>
-        {/* 미리보기 */}
-             {showSlide && previewList.length > 0 && (
-                            <div className="preview-slide">
-                                {previewList.map(item => (
-                                    <div key={item._id} onClick={() => setShowSlide(false)}>
-                                        <SearchItem item={item} keyword={inputText} />
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-                    </div>
-
-                    <div className="header-right">
-                        {loginUser ? (
-                            <>
-                                <span>{loginUser}님</span>
-                                <span className="bar">|</span>
-                                <span onClick={onLogout} style={{ cursor: 'pointer' }}>로그아웃</span>
-                            </>
-                        ) : (
-                            <>
-                                <Link to="/login" style={{ color: 'rgba(255,255,255,0.8)', textDecoration: 'none' }}>로그인</Link>
-                                <span className="bar">|</span>
-                                <Link to="/login" style={{ color: 'rgba(255,255,255,0.8)', textDecoration: 'none' }}>회원가입</Link>
-                            </>
-                        )}
-                    </div>
+          <div className="header-search-wrap">
+                    <input
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+                        placeholder="검색"
+                        className="header-search-input"
+                    />
+                    <button onClick={handleSearch} className="header-search-btn">검색</button>
+                    {/* 미리보기 */}
+                    {showSlide && previewList.length > 0 && (
+                        <div className="preview-slide">
+                            {previewList.map(item => (
+                                <div key={item._id} onClick={() => setShowSlide(false)}>
+                                    <SearchItem item={item} keyword={inputText} />
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </header>
