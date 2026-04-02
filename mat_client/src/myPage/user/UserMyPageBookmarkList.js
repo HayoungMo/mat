e17ec08e0272949e62addebb28bebe78658e9aee
yerinan53-max <yerinan53@gmail.com>
2026-04-02 12:37:13 +0900

    UserMyPage긴급 보수 진행
    -UserMyPage.css 추가
    
    -UserMyPageList.js:
    내가 쓴 글 페이징 처리 및 제목 글자수 표시 제한
    
    -UserMyPageItem.js
    -UserMyPageBookmarkList.js:
    북마크 페이징 처리
    
    UserMyPageItem: 리뷰 제목 글자수 제한을 위한 div 추가

--- mat_client/src/myPage/user/UserMyPageBookmarkList.js
+++ mat_client/src/myPage/user/UserMyPageBookmarkList.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserMyPageBookmarkItem from './UserMyPageBookmarkItem';
 import { useBookmark } from '../../contexts/BookmarkContext';
 
 const UserMyPageBookmarkList = ({onSelectPlace}) => {
     const {bookmarks, onDel} = useBookmark();
+
+     const [page,setPage]=useState(1)
+        const itemPerPage = 5
+    
+        const totalPage = Math.ceil(bookmarks.length / itemPerPage)
+    
+        useEffect(() => {
+            if (page > totalPage){
+                setPage(totalPage || 1)
+            }
+        },[bookmarks, totalPage, page])
+    
+        const startIndex = (page - 1) * itemPerPage
+        const currentItems = bookmarks.slice(startIndex, startIndex + itemPerPage)
    
     return (
         <div>
@@ -30,11 +44,35 @@ const UserMyPageBookmarkList = ({onSelectPlace}) => {
                         bookmarks.length === 0
                         ? <tr><td colSpan='3'>저장된 북마크가 없습니다.</td></tr>
                         :
-                        bookmarks.map(item=><UserMyPageBookmarkItem key={item._id} item={item}
+                       currentItems.map(item=><UserMyPageBookmarkItem key={item._id} item={item}
                          onSelectPlace={onSelectPlace}/>)
                     }
                 </tbody>
             </table>
+
+            {totalPage > 1 && (
+                <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                    {Array.from({ length: totalPage }, (_, i) => i + 1).map((num) => (
+                        <button
+                            key={num}
+                            onClick={() => setPage(num)}
+                            style={{
+                                margin: '0 5px',
+                                padding: '6px 12px',
+                                borderRadius: '6px',
+                                border: page === num ? '2px solid #6D1E2A' : '1px solid #ccc',
+                                background: page === num ? '#6D1E2A' : '#fff',
+                                color: page === num ? '#fff' : '#333',
+                                cursor: 'pointer'
+                            }}
+                        >
+                            {num}
+                        </button>
+                    ))}
+                </div>
+                 )}
+
+
         </div>
     );
 };
--- mat_client/src/myPage/user/UserMyPageList.js
+++ mat_client/src/myPage/user/UserMyPageList.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserMyPageItem from './UserMyPageItem';
 
 const UserMyPageList = ({users,onDel}) => {
+
+    const [page,setPage]=useState(1)
+    const itemPerPage = 5
+
+    const totalPage = Math.ceil(users.length / itemPerPage)
+
+    useEffect(() => {
+        if (page > totalPage){
+            setPage(totalPage || 1)
+        }
+    },[users, totalPage, page])
+
+    const startIndex = (page - 1) * itemPerPage
+    const currentItems = users.slice(startIndex, startIndex + itemPerPage)
 
 
    return (
@@ -25,10 +39,31 @@ const UserMyPageList = ({users,onDel}) => {
                     {users.length === 0
                     ? <tr><td colSpan='3'>게시글이 없습니다.</td></tr>
                     :
-                        users.map(item=><UserMyPageItem key={item._id} item={item} onDel={onDel}/>)
+                       currentItems.map(item=><UserMyPageItem key={item._id} item={item} onDel={onDel}/>)
                     }
                 </tbody>
             </table>
+            {totalPage > 1 && (
+                <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                    {Array.from({ length: totalPage }, (_, i) => i + 1).map((num) => (
+                        <button
+                            key={num}
+                            onClick={() => setPage(num)}
+                            style={{
+                                margin: '0 5px',
+                                padding: '6px 12px',
+                                borderRadius: '6px',
+                                border: page === num ? '2px solid #6D1E2A' : '1px solid #ccc',
+                                background: page === num ? '#6D1E2A' : '#fff',
+                                color: page === num ? '#fff' : '#333',
+                                cursor: 'pointer'
+                            }}
+                        >
+                            {num}
+                        </button>
+                    ))}
+                </div>
+                 )}
         </div>
     );
 };
